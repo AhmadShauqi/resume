3b921a5e331537a41a08a99ed42c0bbfc21a4bb8
Shauqi <ahmadshauqi88@gmail.com>
2024-12-02 17:25:22 +0800

    First time add github backup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+function Experience() {
+  return (
+    <>
+      <h3>Software Developer</h3>
+      <h4>Sigma Lab Sdn Bhd</h4>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h1>Ahmad Shauqi</h1>
+      <h2>Software Developer</h2>
+      <Experience />
+    </>
+  )
+}
+
+
+export default App;
